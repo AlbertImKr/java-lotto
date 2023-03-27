8948877fe36508f79e1e65e5d6b8084b5d991831
ALbertIM0427 <guang7226961@naver.com>
2023-03-28 08:56:08 +0900

    feat: 당첨 번호 객체 추가

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class WinningNumbers {
+	private final Set<LottoNumber> winningNumbers;
+
+	private WinningNumbers(Set<LottoNumber> winningNumbers) {
+		validateSize(winningNumbers);
+		this.winningNumbers = winningNumbers;
+	}
+
+	private void validateSize(Set<LottoNumber> winningNumbers) {
+		if (winningNumbers.size() != 6) {
+			throw new IllegalArgumentException("당첨번호는 서로 다르고 6개 이여야 합니다.");
+		}
+	}
+
+	public static WinningNumbers generateByString(String winningNumbersString) {
+		validateType(winningNumbersString);
+		Set<LottoNumber> winningNumbers = Arrays.stream(winningNumbersString.split(","))
+			.map(s -> new LottoNumber(Integer.parseInt(s)))
+			.collect(Collectors.toSet());
+		return new WinningNumbers(winningNumbers);
+	}
+
+	private static void validateType(String winningNumbersString) {
+		if (!winningNumbersString.matches("^[1-9]?[0-9]*(,[1-9]?[0-9]){5}")) {
+			throw new IllegalArgumentException("잘못된 당첨번호 타입입니다.");
+		}
+	}
+}
