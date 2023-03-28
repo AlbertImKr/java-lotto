5165bd451059d42f70569bea80b8167c1224373f
ALbertIM0427 <guang7226961@naver.com>
2023-03-28 09:35:12 +0900

    feat: 테스트를 하기 위하여 에러 문자열을 상수로 처리

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class WinningNumbers {
+	public static final String TYPE_ERROR = "잘못된 당첨번호 타입입니다.";
+	public static final String SIZE_ERROR = "당첨번호는 서로 다르고 6개 이여야 합니다.";
 	private final Set<LottoNumber> winningNumbers;
 
 	private WinningNumbers(Set<LottoNumber> winningNumbers) {
@@ -14,7 +16,7 @@ public class WinningNumbers {
 
 	private void validateSize(Set<LottoNumber> winningNumbers) {
 		if (winningNumbers.size() != 6) {
-			throw new IllegalArgumentException("당첨번호는 서로 다르고 6개 이여야 합니다.");
+			throw new IllegalArgumentException(SIZE_ERROR);
 		}
 	}
 
@@ -28,7 +30,7 @@ public class WinningNumbers {
 
 	private static void validateType(String winningNumbersString) {
 		if (!winningNumbersString.matches("^[1-9]?[0-9]*(,[1-9]?[0-9]){5}")) {
-			throw new IllegalArgumentException("잘못된 당첨번호 타입입니다.");
+			throw new IllegalArgumentException(TYPE_ERROR);
 		}
 	}
 }
