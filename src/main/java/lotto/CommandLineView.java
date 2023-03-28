8063898275be9596cc0cf4fd212c6e250cb112a1
ALbertIM0427 <guang7226961@naver.com>
2023-03-28 09:44:14 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/lotto/CommandLineView.java
+++ src/main/java/lotto/CommandLineView.java
@@ -20,4 +20,16 @@ public class CommandLineView {
 			return Optional.empty();
 		}
 	}
+
+	public static Optional<WinningNumbers> inputWinningNumbers() {
+		try {
+			System.out.println("당첨 번호를 입력 하세요. 번호는 쉼표(,)를 기준으로 구분해주세요.");
+			Scanner scanner = new Scanner(System.in);
+			String winningNumbers = scanner.nextLine();
+			return Optional.of(WinningNumbers.generateByString(winningNumbers));
+		} catch (IllegalArgumentException e) {
+			System.err.println("잘못 입력했습니다. 번호는 쉼표(,)를 기준으로 구분해주세요.");
+			return Optional.empty();
+		}
+	}
 }
