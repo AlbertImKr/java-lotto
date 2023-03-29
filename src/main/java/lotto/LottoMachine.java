898b68f1da64cb5a73d94d80af46e8e5d77a6fe6
ALbertIM0427 <guang7226961@naver.com>
2023-03-30 07:51:41 +0900

    style: 당첨번호 생성 기능을 LottoMachine에 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoMachine {
+
+	public static final String TYPE_ERROR = "잘못된 당첨번호 타입입니다.";
 
 	private final static List<LottoNumber> allLottoNumbers = IntStream.range(1, 46)
 		.mapToObj(LottoNumber::new)
@@ -21,5 +25,19 @@ public class LottoMachine {
 		List<LottoNumber> lottoNumbers = allLottoNumbers.subList(1, 6);
 		lottoNumbers.sort(Comparator.comparingInt(LottoNumber::getLottoNumber));
 		return new Ticket(lottoNumbers.stream().collect(Collectors.toSet()));
+	}
+
+	public static WinningNumbers generateByString(String winningNumbersString) {
+		validateType(winningNumbersString);
+		Set<LottoNumber> winningNumbers = Arrays.stream(winningNumbersString.split(","))
+			.map(s -> new LottoNumber(Integer.parseInt(s)))
+			.collect(Collectors.toSet());
+		return new WinningNumbers(winningNumbers);
+	}
+
+	private static void validateType(String winningNumbersString) {
+		if (!winningNumbersString.matches("^[1-9]?[0-9]*(,[1-9]?[0-9]){5}")) {
+			throw new IllegalArgumentException(TYPE_ERROR);
+		}
 	}
 }
