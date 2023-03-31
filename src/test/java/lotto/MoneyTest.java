7befc020f055de837375bb3bc4fb1bd2f8b6a1f4
ALbertIM0427 <guang7226961@naver.com>
2023-04-01 08:05:06 +0900

    test: Money 테스트 추가

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -3,7 +3,9 @@ package lotto;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest {
@@ -21,5 +23,21 @@ class MoneyTest {
 	@ValueSource(strings = {"1000", "9000", "121000"})
 	void generateSuccess(String moneyString) {
 		assertThat(new Money(moneyString)).hasFieldOrPropertyWithValue("money", Long.parseLong(moneyString));
+	}
+
+	@DisplayName("티켓 개수를 return")
+	@ParameterizedTest
+	@CsvSource(value = {"1000,1", "9000,9", "200000,200"})
+	void getTicketsQuantity(String moneyString, int exceptCounts) {
+		Money money = new Money(moneyString);
+		assertThat(money.getTicketsQuantity()).isEqualTo(exceptCounts);
+	}
+
+	@DisplayName("수익율 계산 결과를 return")
+	@ParameterizedTest
+	@CsvSource(value = {"1000,1000,0.00", "1000,0,-100.00", "1000,10000,900.00"})
+	void getWiningRate(String moneyString, long winnings, String except) {
+		Money money = new Money(moneyString);
+		assertThat(money.getWiningRate(winnings)).isEqualTo(except);
 	}
 }
