12946649288d7a6dc83ed2c0e53c2f86fff4708e
ALbertIM0427 <guang7226961@naver.com>
2023-04-01 06:53:43 +0900

    refactor: lottoGame 보기 편하게 메서드 분리

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -11,37 +11,68 @@ import java.util.stream.IntStream;
 public class LottoGame {
 
 	void start() {
+		Money money = getMoney();
+
+		List<Lotto> tickets = buyTickets(money);
+
+		printTickets(tickets);
+
+		Lotto winningLotto = getWinningNumbers();
+
+		LottoNumber bonusLottoNumber = getBonusLottoNumber();
+
+		Map<Prize, Long> result = getResult(tickets, winningLotto, bonusLottoNumber);
+
+		printResult(result);
+
+		printWinningRate(money, result);
+	}
+
+	private static Money getMoney() {
 		Optional<Money> optionalMoney = CommandLineView.inputMoney();
 		while (optionalMoney.isEmpty()) {
 			optionalMoney = CommandLineView.inputMoney();
 		}
+		return optionalMoney.get();
+	}
 
-		Money money = optionalMoney.get();
+	private static List<Lotto> buyTickets(Money money) {
 		long ticketsQuantity = money.getTicketsQuantity();
 
 		List<Lotto> tickets = IntStream.iterate(0, i -> i < ticketsQuantity, i -> i + 1)
 			.mapToObj(i -> LottoMachine.generateRandomTicket())
 			.collect(Collectors.toList());
+		return tickets;
+	}
 
+	private static void printTickets(List<Lotto> tickets) {
 		System.out.println(tickets.size() + "개를 구매했습니다.");
 		for (Lotto lotto : tickets) {
 			System.out.println(lotto.getLottoNumber());
 		}
+	}
 
+	private static Lotto getWinningNumbers() {
 		Optional<Lotto> optionalWinningNumbers = CommandLineView.inputWinningNumbers();
 		while (optionalWinningNumbers.isEmpty()) {
 			optionalWinningNumbers = CommandLineView.inputWinningNumbers();
 		}
+		return optionalWinningNumbers.get();
+	}
 
+	private static LottoNumber getBonusLottoNumber() {
 		Optional<LottoNumber> optionalLottoNumber = CommandLineView.inputBonusNumber();
 		while (optionalLottoNumber.isEmpty()) {
 			optionalLottoNumber = CommandLineView.inputBonusNumber();
 		}
+		return optionalLottoNumber.get();
+	}
 
-		Lotto winningLotto = optionalWinningNumbers.get();
-		LottoNumber lottoNumber = optionalLottoNumber.get();
-		Map<Prize, Long> result = LottoChecker.getResult(winningLotto, lottoNumber, tickets);
+	private static Map<Prize, Long> getResult(List<Lotto> tickets, Lotto winningLotto, LottoNumber bonusLottoNumber) {
+		return LottoChecker.getResult(winningLotto, bonusLottoNumber, tickets);
+	}
 
+	private static void printResult(Map<Prize, Long> result) {
 		List<Prize> collect = Arrays.stream(Prize.values())
 			.filter(prize -> prize != Prize.NO)
 			.sorted(Comparator.reverseOrder())
@@ -51,7 +82,9 @@ public class LottoGame {
 			Long quantity = result.getOrDefault(prize, 0L);
 			System.out.println(prize.getDescription() + " - " + quantity + "개");
 		}
+	}
 
+	private static void printWinningRate(Money money, Map<Prize, Long> result) {
 		String rate = WinningCalculator.getWinningRate(result, money);
 		System.out.println("총 수익률은 " + rate + "%입니다.");
 	}
