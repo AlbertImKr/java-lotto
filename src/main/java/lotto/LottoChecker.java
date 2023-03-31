298bf335cd40ba406cb6aac54ae22fc92af29034
ALbertIM0427 <guang7226961@naver.com>
2023-04-01 06:54:00 +0900

    style: 네이밍 수정

--- src/main/java/lotto/LottoChecker.java
+++ src/main/java/lotto/LottoChecker.java
@@ -5,16 +5,16 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoChecker {
-	public static Map<Prize, Long> getResult(Lotto winningLotto, LottoNumber lottoNumber, List<Lotto> tickets) {
+	public static Map<Prize, Long> getResult(Lotto winningLotto, LottoNumber bonusLottoNumber, List<Lotto> tickets) {
 		return tickets.stream()
-			.map(ticket -> LottoChecker.getResult(winningLotto, lottoNumber, ticket))
+			.map(ticket -> LottoChecker.getResult(winningLotto, bonusLottoNumber, ticket))
 			.collect(Collectors.groupingBy(prize -> prize, Collectors.counting()));
 	}
 
-	private static Prize getResult(Lotto winningLotto, LottoNumber lottoNumber, Lotto ticket) {
+	private static Prize getResult(Lotto winningLotto, LottoNumber bonusLottoNumber, Lotto ticket) {
 		int matchCount = winningLotto.getMatchCount(ticket);
 		Prize prize = Prize.getPrize(matchCount);
-		if (prize == Prize.SECOND && ticket.contains(lottoNumber)) {
+		if (prize == Prize.SECOND && ticket.contains(bonusLottoNumber)) {
 			return Prize.BONUS;
 		}
 		return prize;
